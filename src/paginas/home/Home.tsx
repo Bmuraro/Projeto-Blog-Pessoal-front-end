81bfcf7ab6c6b9d7b0c8ac601677cc683fd32679
Bruno Muraro <bmuraro3@gmail.com>
2021-10-25 16:46:12 -0300

    Iniciando React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem da tela inicial" className='img'/>
+        </>
+    );
+}
+
+export default Home;
